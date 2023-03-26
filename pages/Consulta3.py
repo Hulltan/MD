95e012e72e6081f9b229768f26d726acd8613e29
Jesulen Vicente <vicente.jesulen@gmail.com>
2023-03-26 20:28:05 -0300

    Initial commit

---
+++ pages/Consulta3.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+
+# ???
+# Qual é a relação entre o desempenho 
+# dos estudantes no ENEM e sua origem étnica e racial nos bairros do Recife?
+
+st.write("## Consulta 3")
+st.write('##### Descrição')
